553e087cc21d1345b8202016b6c4e9efe5c38538
alexanderthecreator <alexander.rastegaev@gmail.com>
2023-01-18 17:48:17 +0500

    modify README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/alexanderthecreator/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/alexanderthecreator/python-project-50/actions)
+<a href="https://codeclimate.com/github/alexanderthecreator/python-project-50/maintainability"><img src="https://api.codeclimate.com/v1/badges/b975a0a03abbc94c1b8a/maintainability" /></a>
+
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -22,11 +22,11 @@ def generate_diff(file_path1, file_path2):
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
+    parser.add_argument('file_path1')
+    parser.add_argument('file_path2')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    generate_diff(args.first_file, args.second_file)
+    generate_diff(args.file_path1, args.file_path2)
 
 
 if __name__ == "__main__":
