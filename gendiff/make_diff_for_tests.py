ba326296c659264d5b4b39979f1ecd7706bdcd15
alexanderthecreator <alexander.rastegaev@gmail.com>
2023-02-01 00:51:52 +0500

    modify  gendiff/make_diff_for_tests.py

--- gendiff/make_diff_for_tests.py
+++ gendiff/make_diff_for_tests.py
@@ -10,10 +10,23 @@ def get_file(file_path):
         return json.load(open(file_path))
 
 
+'''def make_diff(first_file, second_file):
+    first_file_keys = set(zip(first_file.keys(), first_file.values()))
+    second_file_keys = set(zip(second_file.keys(), second_file.values()))
+    list1 = [{key: value, 'meta': 'no_changes'} for key, value in first_file.items() if
+             (key, value) in first_file_keys & second_file_keys]
+    list2 = [{key: value, 'meta': 'remove'} for key, value in first_file.items() if
+             (key, value) in first_file_keys - second_file_keys]
+    list3 = [{key: value, 'meta': 'add'} for key, value in first_file.items() if
+             (key, value) in second_file_keys - first_file_keys]
+    result_list = list1 + list2 + list3
+    return result_list'''
+
+
 def make_diff(first_file, second_file):
     """Make a diff between two dicts"""
     diff_ = set(first_file) | set(second_file)
-    print(diff_)
+    # print(diff_)
     list_of_dicts = []
     for item in diff_:
         if item in first_file and item in second_file:
@@ -22,10 +35,12 @@ def make_diff(first_file, second_file):
             else:
                 list_of_dicts.append({item: first_file[item], 'meta': 'remove'})
                 list_of_dicts.append({item: second_file[item], 'meta': 'add'})
-        elif item in first_file and item not in second_file:
+        if item in first_file and item not in second_file:
             list_of_dicts.append({item: first_file[item], 'meta': 'remove'})
-        elif item in second_file and item not in first_file:
+        if item in second_file and item not in first_file:
             list_of_dicts.append({item: second_file[item], 'meta': 'add'})
+        if type(first_file.get(item)) == dict and type(second_file.get(item)) == dict:
+            make_diff(first_file[item], second_file[item])
     print(list_of_dicts)
     return list_of_dicts
 
@@ -54,3 +69,5 @@ def generate_diff(file_path1, file_path2):
 f_p1 = '/home/alexander/PycharmProjects/file4.json'
 f_p2 = '/home/alexander/PycharmProjects/file5.json'
 generate_diff(f_p1, f_p2)
+
+
