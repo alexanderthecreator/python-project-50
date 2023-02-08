c6eb1fe13391014da44694c742fe31cf4ac044ac
alexanderthecreator <alexander.rastegaev@gmail.com>
2023-02-08 14:36:13 +0500

    Just a test of functionality

--- gendiff/make_diff_for_tests.py
+++ gendiff/make_diff_for_tests.py
@@ -10,47 +10,40 @@ def get_file(file_path):
         return json.load(open(file_path))
 
 
-
-def make_diff(first_dict, second_dict, result_diff = []):
+def make_diff(first_dict, second_dict):
     """Make a diff between two dicts"""
-    if type(first_dict) == dict and type(second_dict) == dict:
-        dict_set = set(first_dict) | set(second_dict)
-        for item in dict_set:
-            if item in first_dict and item in second_dict:
-                if first_dict[item] == second_dict[item] and type(first_dict[item]) == dict \
-                        and type(second_dict[item]) == dict:
-                    result_diff.append({'key': item, 'value': first_dict[item], 'meta': 'no_changes'})
-                elif first_dict[item] == second_dict[item]:
-                    result_diff.append({'key': item, 'value': first_dict[item], 'meta': 'no_changes'})
-                elif first_dict[item] != second_dict[item] and type(first_dict[item]) == dict \
-                        and type(second_dict[item]) == dict:
-                    result_diff.append({'key': item, 'value': first_dict[item], 'meta': 'nested'})
+    result_diff = []
+    for key in set(first_dict).union(second_dict):
+        if key in first_dict and key in second_dict:
+            if type(first_dict[key]) == dict and type(second_dict[key]) == dict:
+                if first_dict[key] == second_dict[key]:
+                    result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'no_changes'})
+                if first_dict[key] != second_dict[key]:
+                    #result_diff.append({'key1': key, 'value': first_dict[key], 'meta': 'nested'})
+                    #result_diff.append({'key2': key, 'value': second_dict[key], 'meta': 'nested'})
+                    make_diff(first_dict[key], second_dict[key])
+            else:
+                if first_dict[key] == second_dict[key]:
+                    result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'no_changes'})
                 else:
-                    result_diff.append({'key': item, 'value': first_dict[item], 'meta': 'remove'})
-                    result_diff.append({'key': item, 'value': second_dict[item], 'meta': 'add'})
-                make_diff(first_dict[item], second_dict[item], result_diff=result_diff)
+                    result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'remove'})
+                    result_diff.append({'key': key, 'value': second_dict[key], 'meta': 'add'})
+        if key in first_dict and key not in second_dict:
+            result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'remove'})
+        if key in second_dict and key not in first_dict:
+            result_diff.append({'key': key, 'value': second_dict[key], 'meta': 'add'})
 
-            if item in first_dict and item not in second_dict:
-                result_diff.append({'key': item, 'value': first_dict[item], 'meta': 'remove'})
-            if item in second_dict and item not in first_dict:
-                result_diff.append({'key': item, 'value': second_dict[item], 'meta': 'add'})
-
-    print(result_diff)
+    #print(result_diff)
     return result_diff
 
 
-def generate_diff(file_path1, file_path2):
-    """Make a string from list of diff"""
-    first_file = get_file(file_path1)
-    second_file = get_file(file_path2)
+def stylish(str_):
     diff_string = ''
-    str_ = make_diff(first_file, second_file)
-    #print(str_)
     for item in str_:
         if item['meta'] == 'no_changes':
             diff_string += f"    {item.get('key')}: {item.get('value')}\n"
         if item['meta'] == 'nested':
-            diff_string += f"    {item.get('key')}: {item.get('value')}\n"
+            diff_string += stylish (item['value'])
         if item['meta'] == 'add':
             diff_string += f"  + {item.get('key')}: {item.get('value')}\n"
         if item['meta'] == 'remove':
@@ -60,9 +53,22 @@ def generate_diff(file_path1, file_path2):
     return final_diff_string
 
 
+def generate_diff(file_path1, file_path2):
+    """Make a string from list of diff"""
+    first_file = get_file(file_path1)
+    second_file = get_file(file_path2)
+    str_ = make_diff(first_file, second_file)
+    print(str_)
+    return stylish(str_)
+
+
+
+
 f_p1 = '/home/alexander/PycharmProjects/file4.json'
 f_p2 = '/home/alexander/PycharmProjects/file5.json'
 
+
 generate_diff(f_p1, f_p2)
+
 
 
