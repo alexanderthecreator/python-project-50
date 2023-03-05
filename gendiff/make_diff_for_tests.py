f94a331b4bc90fd01be4397c16d577d794dc9b41
alexanderthecreator <alexander.rastegaev@gmail.com>
2023-03-05 23:50:10 +0500

    modify gendiff/make_diff_for_tests.py

--- gendiff/make_diff_for_tests.py
+++ gendiff/make_diff_for_tests.py
@@ -9,48 +9,63 @@ def get_file(file_path):
     elif '.json' in file_path:
         return json.load(open(file_path))
 
-result_diff = []
-def make_diff(first_dict, second_dict, rec_depth=0):
-    """Make a diff between two dicts"""
 
+def make_diff(first_dict, second_dict):
+    result_diff = []
     for key in set(first_dict).union(second_dict):
         if key in first_dict and key not in second_dict:
-            result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'remove', 'depth': rec_depth})
+            result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'remove'})
         if key in second_dict and key not in first_dict:
-            result_diff.append({'key': key, 'value': second_dict[key], 'meta': 'add', 'depth': rec_depth})
+            result_diff.append({'key': key, 'value': second_dict[key], 'meta': 'add'})
         if key in first_dict and key in second_dict:
             if first_dict[key] == second_dict[key]:
-                result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'no_changes', 'depth': rec_depth})
+                result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'unchanged'})
             if first_dict[key] != second_dict[key]:
                 if isinstance(first_dict[key], dict) and isinstance(second_dict[key], dict):
-                    result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'nested', 'depth': rec_depth})
-                    result_diff.append({'key': key, 'value': second_dict[key], 'meta': 'nested', 'depth': rec_depth})
-                    make_diff(first_dict[key], second_dict[key], rec_depth+1)
-
+                    sub_res = (make_diff(first_dict[key], second_dict[key]))
+                    if sub_res:
+                        result_diff.append({'key': key, 'value': sub_res, 'meta': 'nested'})
                 else:
-                    result_diff.append({'key': key, 'value': first_dict[key], 'meta': 'remove', 'depth': rec_depth})
-                    result_diff.append({'key': key, 'value': second_dict[key], 'meta': 'add', 'depth': rec_depth})
-                    return result_diff
-
+                    result_diff.append({'key': key, 'old_value': first_dict[key], 'new_value': second_dict[key], 'meta': 'nested'})
     #print(result_diff)
     return result_diff
 
 
-def stylish(str_):
-    """Format result diff string"""
-    diff_string = ''
-    for item in str_:
-        if item['meta'] == 'no_changes':
-            diff_string += f"    {item.get('key')}: {item.get('value')}\n"
-        if item['meta'] == 'add':
-            diff_string += f"  + {item.get('key')}: {item.get('value')}\n"
-        if item['meta'] == 'remove':
-            diff_string += f"  - {item.get('key')}: {item.get('value')}\n"
-        if item['meta'] == 'nested':
-            diff_string += f"    {item.get('key')}: \n"
-    final_diff_string = "{\n" + diff_string.lower() + "}"
-    print(final_diff_string)
-    return final_diff_string
+def format_result(result):
+    sorted_result = sorted(result, key=lambda x: x["key"])
+    formatted = "{\n"
+    for item in sorted_result:
+        key = item["key"]
+        meta = item["meta"]
+        value = item.get("value")
+        old_value = item.get("old_value")
+        new_value = item.get("new_value")
+        if isinstance(value, list):
+            value = format_result(value)
+        if meta == "add":
+            formatted += f"  + {key}: {value}\n"
+        elif meta == "remove":
+            formatted += f"  - {key}: {value}\n"
+        elif meta == "unchanged":
+            formatted += f"    {key}: {value}\n"
+        elif meta == "nested":
+
+            if isinstance(old_value, dict):
+                old_value_str = format_result(make_diff(old_value, new_value))
+            else:
+                old_value_str = str(old_value)
+
+            if isinstance(new_value, dict):
+                new_value_str = format_result(make_diff(new_value, old_value))
+            else:
+                new_value_str = str(new_value)
+
+            formatted += f"  - {key}: {old_value_str} \n"
+            formatted += f"  + {key}: {new_value_str}\n"
+
+    formatted += "}"
+    print (formatted)
+    return formatted
 
 
 def generate_diff(file_path1, file_path2):
@@ -59,7 +74,7 @@ def generate_diff(file_path1, file_path2):
     second_file = get_file(file_path2)
     str_ = make_diff(first_file, second_file)
     print(str_)
-    return stylish(str_)
+    return format_result(str_)
 
 
 f_p1 = '/home/alexander/PycharmProjects/file4.json'
