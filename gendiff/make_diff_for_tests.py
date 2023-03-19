b34e6f689bcf1286740cc182bfe4d32193f2c2ef
alexanderthecreator <alexander.rastegaev@gmail.com>
2023-03-19 22:13:57 +0500

    modify gendiff/make_diff_for_tests.py

--- gendiff/make_diff_for_tests.py
+++ gendiff/make_diff_for_tests.py
@@ -11,6 +11,9 @@ def get_file(file_path):
 
 
 def make_diff(first_dict, second_dict):
+    """
+    Generate a diff between two nested dictionaries
+    """
     result_diff = []
     for key in set(first_dict).union(second_dict):
         if key in first_dict and key not in second_dict:
