042bcae35af3c5ae1394ead3ef28054a9f9d42a8
alexanderthecreator <alexander.rastegaev@gmail.com>
2023-04-10 22:18:55 +0500

    FileOpenError is fixed

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -6,11 +6,11 @@ from gendiff.scripts.gendiff import generate_diff
 @pytest.fixture()
 def get_test_jsons():
     """Get two JSON files address"""
-    first_file_address = '/home/alexander/PycharmProjects/file1.json'
-    second_file_address = '/home/alexander/PycharmProjects/file2.json'
+    first_file_address = './fixtures/file1.json'
+    second_file_address = './fixtures/file2.json'
     return first_file_address, second_file_address
 
 
 def test_generate_diff(get_test_jsons):
-    f = open('/home/alexander/PycharmProjects/python-project-50/tests/fixtures/result_string.txt')
+    f = open('./fixtures/result_string.txt')
     assert generate_diff(*get_test_jsons) == f.read()
